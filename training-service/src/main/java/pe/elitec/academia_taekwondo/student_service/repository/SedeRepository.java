eaf3c9f1a416d7c650d443ef07787dfc5f9a3e38
XxAbiudxX <abiud.arreaza.blanco@gmail.com>
2026-04-01 19:40:04 -0500

    no se que poner solo se que no se nada

--- training-service/src/main/java/pe/elitec/academia_taekwondo/student_service/repository/SedeRepository.java
+++
@@ -1,9 +0,0 @@
-package pe.elitec.academia_taekwondo.student_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import pe.elitec.academia_taekwondo.student_service.entity.Sede;
-
-@Repository
-public interface SedeRepository extends JpaRepository<Sede, Long> {
-}
